7c75fd414fecd9ff84d35850a186e64ffc346d3a
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2025-12-25 22:06:31 +0300

    feat: adds language switcher (#5)

---
+++ src/widgets/LanguageSwitcher/index.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { usePathname, useRouter } from 'next/navigation';
+
+export function LanguageSwitcher() {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const switchLocale = (locale: string) => {
+        const segments = pathname.split('/');
+        segments[1] = locale;
+        router.push(segments.join('/'));
+    };
+
+    return (
+        <>
+            <button onClick={() => switchLocale('en-US')}>EN</button>
+            <button onClick={() => switchLocale('ru-RU')}>RU</button>
+        </>
+    );
+}
