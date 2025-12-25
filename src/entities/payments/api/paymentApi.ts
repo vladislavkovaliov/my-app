619b0db64172a8bcc8b1ac6aac95fed0ca715f8c
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2025-12-26 02:20:56 +0300

    feat: fixs comments

--- src/entities/payments/api/paymentApi.ts
+++ src/entities/payments/api/paymentApi.ts
@@ -2,5 +2,5 @@ import { Payment } from '../model/payment';
 import { fetcher } from '@/shared/api/fetcher';
 
 export const getPayments = async (): Promise<{ total: number; data: Payment[] }> => {
-    return fetcher('/api/payments');
+    return await fetcher('/api/payments');
 };
