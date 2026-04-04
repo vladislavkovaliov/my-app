9bd12352c8cd599abccb7cb7f4102110fe01da15
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2026-04-04 21:17:48 +0300

    feat: [UI-0000] up semantic release version

--- release.config.cjs
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const { getPlugins } = require('./config/semantic-release/plugins.cjs');
-
-module.exports = {
-    plugins: getPlugins('Bug Fixes'),
-    branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main'],
-};
