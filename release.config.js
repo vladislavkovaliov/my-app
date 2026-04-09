43f2806935c285d82aa1bdd044627d17eb94eea5
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2026-04-10 00:25:52 +0300

    fix: [UI-0000] simulate hot fix (#11)

--- release.config.js
+++ release.config.js
@@ -46,7 +46,6 @@ module.exports = {
         ],
         '@semantic-release/changelog',
         '@semantic-release/npm',
-        '@semantic-release/git',
         '@semantic-release/exec',
         [
             '@semantic-release/github',
